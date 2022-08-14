09ecb6ba229e106013ec3fa607715c8e8dd106e3
Hilgard <hilgardvr@gmail.com>
2022-08-14 19:46:56 +0200

    add ack.vim

--- init.lua
+++ init.lua
@@ -10,10 +10,14 @@ vim.keymap.set('n', '<F6>', ':buffers<CR>:bd<Space>')
 vim.keymap.set('n', '<C-]>', '<Plug>(coc-definition)')
 vim.keymap.set('n', 'gr', '<Plug>(coc-references)')
 
+-- file searching with ack
+vim.keymap.set('n', '<C-f>', ':Ack! ')
+
 --plugings - run :PlugInstall to install plugings
 local Plug  = vim.fn['plug#']
 vim.call('plug#begin', '~/.config/nvim/plugged')
 Plug('neoclide/coc.nvim', {branch = 'release'})
 Plug('elixir-lsp/coc-elixir', {['do'] = vim.fn['yarn install && yarn prepack']})
 Plug('elixir-editors/vim-elixir')
+Plug('mileszs/ack.vim')
 vim.call('plug#end')
