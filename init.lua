34cf658f4a411587fdc22edf3c8fc465459f3826
Hilgard <hilgardvr@gmail.com>
2022-08-14 19:13:25 +0200

    add coc nov shortcuts

--- init.lua
+++ init.lua
@@ -6,12 +6,14 @@ vim.keymap.set('n', '<C-l>', ':buffers<CR>:buffer<Space>')
 --display and delete buffers with F6
 vim.keymap.set('n', '<F6>', ':buffers<CR>:bd<Space>')
 
+-- Navigation
+vim.keymap.set('n', '<C-]>', '<Plug>(coc-definition)')
+vim.keymap.set('n', 'gr', '<Plug>(coc-references)')
+
+--plugings - run :PlugInstall to install plugings
 local Plug  = vim.fn['plug#']
-
 vim.call('plug#begin', '~/.config/nvim/plugged')
-
-Plug('elixir-lsp/coc-elixir', {['do'] = vim.fn['yarn install && yarn prepack']})
 Plug('neoclide/coc.nvim', {branch = 'release'})
+Plug('elixir-lsp/coc-elixir', {['do'] = vim.fn['yarn install && yarn prepack']})
 Plug('elixir-editors/vim-elixir')
-
 vim.call('plug#end')
