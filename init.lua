a6e2185e6c170db4566d16b3f431644e4bf28009
Hilgard <hilgardvr@gmail.com>
2022-08-16 03:15:44 +0200

    init.lua updates

--- init.lua
+++ init.lua
@@ -1,17 +1,21 @@
 --setup fzf
 vim.o.rtp=vim.o.rtp .. ",/usr/bin/fzf"
-vim.keymap.set('n', '<C-p>', ':FZF<CR>')
+vim.keymap.set('n', '<C-p>', ':Files<CR>')
+vim.keymap.set('n', '<C-f>', ':Rg<CR>')
 --display and switch buffers with C-l
 vim.keymap.set('n', '<C-l>', ':buffers<CR>:buffer<Space>')
 --display and delete buffers with F6
 vim.keymap.set('n', '<F6>', ':buffers<CR>:bd<Space>')
+
+--set Y to vim legacy
+vim.keymap.set('n', 'Y', 'Y')
 
 -- Navigation
 vim.keymap.set('n', '<C-]>', '<Plug>(coc-definition)')
 vim.keymap.set('n', 'gr', '<Plug>(coc-references)')
 
 -- file searching with ack
-vim.keymap.set('n', '<C-f>', ':Ack! ')
+-- vim.keymap.set('n', '<C-f>', ':Ack! ')
 
 --plugings - run :PlugInstall to install plugings
 local Plug  = vim.fn['plug#']
@@ -20,4 +24,7 @@ Plug('neoclide/coc.nvim', {branch = 'release'})
 Plug('elixir-lsp/coc-elixir', {['do'] = vim.fn['yarn install && yarn prepack']})
 Plug('elixir-editors/vim-elixir')
 Plug('mileszs/ack.vim')
+Plug('junegunn/fzf', {['do'] = vim.fn['fzf#install(}']})
+Plug('junegunn/fzf.vim')
+Plug('tpope/vim-fugitive')
 vim.call('plug#end')
