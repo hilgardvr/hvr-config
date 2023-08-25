d5e1b65aa573e57f6105271d5f55fd7d9ac049ec
hilgard <hilgardvr@gmail.com>
2023-08-25 23:05:14 +0200

    update link.sh

--- link.sh
+++ link.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 rm ~/.vimrc || true
 rm ~/.tmux.conf || true
-rm ~/.config/nvim/init.* || true
+rm -rf ~/.config/nvim || true
 rm ~/.zshrc || true
 ln -s $(pwd)/tmux.conf ~/.tmux.conf
-mkdir -p ~/.config/nvim/
 ln -s $(pwd)/vimrc ~/.vimrc
-ln -s $(pwd)/init.lua ~/.config/nvim/init.lua
+ln -s $(pwd)/nvim ~/.config
 ln -s $(pwd)/zshrc ~/.zshrc
