ea3fde6d4d45201d7448d5b4e1643aafe868f32d
hilgard <hilgardvr@gmail.com>
2024-03-23 19:48:35 +0200

    Fix: clipboard

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -5,17 +5,4 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
-vim.o.clipboard="unnamed,unnamedplus"
-
---vim.g.clipboard = {
---    'name': 'myClipboard',
---    'copy': {
---       '+': ['tmux', 'load-buffer', '-'],
---       '*': ['tmux', 'load-buffer', '-'],
---     },
---    'paste': {
---       '+': ['tmux', 'save-buffer', '-'],
---       '*': ['tmux', 'save-buffer', '-'],
---    },
---    'cache_enabled': 1,
---  }
+--vim.o.clipboard="unnamed,unnamedplus"
