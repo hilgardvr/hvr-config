2b297ff8d8be62ecdc17fd98afff6405c4bbf940
hilgard <hilgardvr@protonmail.com>
2024-07-01 15:10:31 +0200

    Merge

--- link.sh
+++ link.sh
@@ -3,6 +3,7 @@
 rm ~/.vimrc || true
 rm ~/.tmux.conf || true
 rm -rf ~/.config/nvim || true
+rm -rf ~/.local/share/nvim || true
 rm ~/.zshrc || true
 ln -s $(pwd)/tmux.conf ~/.tmux.conf
 ln -s $(pwd)/vimrc ~/.vimrc
