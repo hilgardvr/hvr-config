63d7d027bea1b7098abdca73fa61c878d8125e37
hilgard <hilgardvr@protonmail.com>
2024-08-10 13:51:42 +0200

    Rm lsps from ensure

--- nvim/init.lua
+++ nvim/init.lua
@@ -580,7 +580,7 @@ require("lazy").setup({
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			local servers = {
 				-- clangd = {},
-				gopls = {},
+				-- gopls = {},
 				-- pyright = {},
 				-- rust_analyzer = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
@@ -589,11 +589,11 @@ require("lazy").setup({
 				--    https://github.com/pmizio/typescript-tools.nvim
 				--
 				-- But for many setups, the LSP (`tsserver`) will work just fine
-				tsserver = {},
+				-- tsserver = {},
 				--
-				hls = {},
-				kotlin_language_server = {},
-        ocamllsp = {},
+				-- hls = {},
+				-- kotlin_language_server = {},
+        -- ocamllsp = {},
 
 				lua_ls = {
 					-- cmd = {...},
